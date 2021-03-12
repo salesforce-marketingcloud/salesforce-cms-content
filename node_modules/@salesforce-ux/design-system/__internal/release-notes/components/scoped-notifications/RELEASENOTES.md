0cbdfec7677934f1a073e22ff349a70b6d24323a
muenzpraeger <mail@winkelmeyer.com>
2021-03-12 11:53:23 +0100

    fix: remove not needed files

--- node_modules/@salesforce-ux/design-system/__internal/release-notes/components/scoped-notifications/RELEASENOTES.md
+++
@@ -1,7 +0,0 @@
-<!-- Release notes authoring guidelines: http://keepachangelog.com/ -->
-
-# Scoped Notifications Release Notes
-
-<!-- ## [Unreleased] -->
-
-<!-- ## [VERSION] -->
--- node_modules/@salesforce-ux/design-system/design-tokens/dist/form-factor-medium.common.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  /* Unitless line-heights for reusability */
-  lineHeightText: "1.375",
-  /* Variable unitless line-heights for reusability */
-  varLineHeightText: "1.375",
-  widthStageLeft: "250px",
-};
